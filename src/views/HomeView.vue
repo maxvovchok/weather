3ede84e1767db83419cda8895a4055ffcdbbc1e8
maxvovchok <vovchokmakcim@gmail.com>
2024-11-11 18:37:00 +0100

    chore: initialize project with dependencies

---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="home">asd</div>
+</template>
+
+<script></script>
+
+<style></style>
---
+++ src/views/SelectedView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">qwe</div>
+</template>
+
+<script></script>
+
+<style></style>
